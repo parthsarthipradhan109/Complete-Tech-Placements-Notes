e17fb1c58af5dcbc8a6c61976c7a65bcee28ac53
parthsarthipradhan109 <parthsarthipradhan109@gmail.com>
2026-06-07 17:28:41 +0530

    Add Relational Operators example in C++

---
+++ 01_C++_DSA/L-02_Variables_DataTypes_Operators/codes/L02_operators_relational.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    // Operators
+    // 2. Relational Operators
+    int a = 10, b = 5;
+    
+    cout << "For a = " << a << ", b = " << b << endl;
+    cout << "a > b = " << (a > b) << endl; // Greater than
+    cout << "a < b = " << (a < b) << endl; // Less than
+    cout << "a >= b = " << (a >= b) << endl; // Greater than or equal to
+    cout << "a <= b = " << (a <= b) << endl; // Less than or equal to
+    cout << "a == b = " << (a == b) << endl; // Equal to
+    cout << "a != b = " << (a != b) << endl; // Not equal to
+    
+    return 0;
+}
