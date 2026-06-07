cd353ccf2f64d2743b42c52e2a7463aa2cfd97bd
parthsarthipradhan109 <parthsarthipradhan109@gmail.com>
2026-06-08 04:56:26 +0530

    Add Sum of Two Numbers practice problem

---
+++ 01_C++_DSA/L-02_Variables_DataTypes_Operators/codes/L02_sum_of_two_numbers.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    // Sum of two numbers
+    int a, b;
+    cout <<"Enter a :" ;
+    cin >> a;
+    
+    cout <<"Enter b :" ;
+    cin >> b;
+    
+    cout << "sum = " << a + b << endl;
+    
+    return 0;
+}
